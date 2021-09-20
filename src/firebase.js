746d65789ee8c8d5eba16fde6bac77740cd64597
Lokesh Pathrabe <73941213+lokeshpathrabe22@users.noreply.github.com>
2021-09-20 18:07:28 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBQKthiLMsQs0nJnaeKcwh2X7MxHTYqwd4",
-    authDomain: "clone-a82ad.firebaseapp.com",
-    projectId: "clone-a82ad",
-    storageBucket: "clone-a82ad.appspot.com",
-    messagingSenderId: "636808629496",
-    appId: "1:636808629496:web:dadd5697682bcd6e87ff0d",
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
   };
 
   const fire=firebase.initializeApp(firebaseConfig);
